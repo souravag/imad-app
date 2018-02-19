0186552a9a7144abad158adc16d249b3c12ca787
souravag <36446375+souravag@users.noreply.github.com>
2018-02-19 14:54:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.get('/article-one', function(req,res){
 
 app.get('/article-two', function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-  res.send('Article two requested and will be served here'); 
 });
 
 app.get('/article-three', function(req,res){
