9a525da08c41acfa552c19dc7436975bf15f9490
souravag <36446375+souravag@users.noreply.github.com>
2018-04-22 20:43:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,16 +92,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function(req,res){
-    //articleName == article-one
-    var articleName= req.params.articleName;
-    res.send(createTemp(articles[articleName]));
-});
 var counter=0;
 app.get('/counter', function(req,res) {
     counter = counter+1;
     res.send(counter.toString());
 });
+
+app.get('/:articleName', function(req,res){
+    //articleName == article-one
+    var articleName= req.params.articleName;
+    res.send(createTemp(articles[articleName]));
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
