08ad8c7d652857512f5ddde7a39d9aafabf27da9
souravag <36446375+souravag@users.noreply.github.com>
2018-04-22 20:53:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,3 +10,11 @@ function moveRight()
 img.onclick = function(){
     var interval = setInterval(moveRight,50);
 };
+
+var button=document.getElementById('counter');
+button.onClick = function() {
+  
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+};
