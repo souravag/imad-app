0974c55a8e6f5d6ea51e5d39448716b81ec6e8f0
souravag <36446375+souravag@users.noreply.github.com>
2018-04-23 14:55:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,14 +16,3 @@ button.onClick = function() {
   request.send(null);
 };
 console.log('Loaded!');
-
-var img = document.getElementById('imageico');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft= marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
-};
