bccca9ae860c188e9d6667cb9120b6ac785457e1
souravag <36446375+souravag@users.noreply.github.com>
2018-04-23 14:57:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ button.onClick = function() {
         if(request.status===200){
             var counter=request.responseText;
             var span=document.getElementById('count');
-            span.innerHTML=counter.toString();
+            span.innerHTML="Many";
         }
  }
  
