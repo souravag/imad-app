c286529e1bdc4c3bb969fd409134fda772b8f5ad
souravag <36446375+souravag@users.noreply.github.com>
2018-05-01 18:12:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var config = {
+    user:'souravagarwal54321',
+    database:'souravagarwal54321',
+    host:'db.imad.hasura.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 var articles = {
@@ -95,6 +102,20 @@ function createTemp(data) {
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req,res) {
+    pool.query('SELECT * FROM test',function(err,result) {
+       if(err)
+       {
+           res.status(500).send(err.toString());
+       }
+       else
+       {
+           res.send(JSON.stringify(result));
+       }
+    });
 });
 
 var counter=0;
