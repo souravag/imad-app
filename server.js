57691c14b7047776c5d691fc882d4ca6cc9a4357
souravag <36446375+souravag@users.noreply.github.com>
2018-05-06 20:04:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,6 +107,7 @@ app.get('/', function (req, res) {
 var pool = new Pool(config);
 app.get('/test-db', function(req,res) {
     pool.query('SELECT * FROM test',function(err,result) {
+        console.log('Database loaded!');
        if(err)
        {
            res.status(500).send(err.toString());
