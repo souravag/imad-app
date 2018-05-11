88b7ec64aff7e3f5fb6c1ca8de993500ec9587c3
souravag <36446375+souravag@users.noreply.github.com>
2018-05-11 19:28:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool = require('pg').Pool;
 var config = {
     user:'souravagarwal54321',
     database:'souravagarwal54321',
-    host:'db.imad.hasura.io',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password: process.env.DB_PASSWORD
 };
