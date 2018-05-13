425de7b3af95caecd02aad09477b384b7689f0af
souravag <36446375+souravag@users.noreply.github.com>
2018-05-13 19:53:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,36 @@
+
+
+var btn = document.getElementById('login_btn'); 
+btn.onclick = function() {
+ var request = new XMLHttpRequest();
+ request.onreadystatechange = function() {
+    if(request.readyState===XMLHttpRequest.DONE)
+    {
+        if(request.status===200){
+           console.log('User logged in!');
+           alert('Login Success!');
+        }
+        else if(request.status===403)
+        {
+            alert('incorrect credentials!');
+        }
+        else if(request.status==500)
+        {
+            alert('Something went wrong! Try again!');
+        }
+ }
+ 
+  };
+  var user=document.getElementById('username').value;
+  var pass=document.getElementById('password').value;
+  console.log('Username and password received!');
+  request.open('POST', 'http://souravagarwal54321.imad.hasura-app.io/login',true);
+  request.setRequestHeader('Content-Type':'application/json');
+  request.send(JSON.stringify({username: user,password:pass}));
+};
+console.log('Loaded!');
+
+
 var button = document.getElementById('counter'); 
 button.onclick = function() {
  var request = new XMLHttpRequest();
